934a0e9c1aa2651bf4075e12ffb5b7993897eebb
Admin <admin@asiot.net>
2023-01-31 02:56:34 +0530

    first commit

---
+++ letzpay_integration/letzpay_integration/doctype/letzpay_settings/letzpay_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Stya and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Letzpay Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ letzpay_integration/letzpay_integration/doctype/letzpay_settings/test_letzpay_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Stya and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLetzpaySettings(FrappeTestCase):
+	pass
