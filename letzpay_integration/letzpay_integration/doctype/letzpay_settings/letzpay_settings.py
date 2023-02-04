43d4b174f54dd6c9ae7bea520d5e0a64f4a3dbb5
divakar jadoun <divakarjadoun786@gmail.com>
2023-02-05 03:16:41 +0530

    latzpay intigration changes

--- letzpay_integration/letzpay_integration/doctype/letzpay_settings/letzpay_settings.py
+++ letzpay_integration/letzpay_integration/doctype/letzpay_settings/letzpay_settings.py
@@ -4,40 +4,18 @@
 import frappe
 from frappe.model.document import Document
 from payments.utils import create_payment_gateway
-import hashlib
-import hmac
-import json
-from urllib.parse import urlencode
 from frappe import _
 from frappe.integrations.utils import (
 	create_request_log,
-	make_get_request,
-	make_post_request,
 )
 from frappe.model.document import Document
-from frappe.utils import call_hook_method, cint, get_timestamp, get_url
+from frappe.utils import call_hook_method, cint, get_url
 
 class LetzpaySettings(Document):
 	supported_currencies = ["INR"]
 	def validate(self):
 		create_payment_gateway("Letzpay")
 		call_hook_method("payment_gateway_enabled", gateway="Letzpay")
-		if not self.flags.ignore_mandatory:
-			self.validate_letzpay_credentails()
-
-	def validate_letzpay_credentails(self):
-		if self.pay_id and self.salt and self.encryption_key:
-			pass
-			# try:
-			# 	make_get_request(
-			# 		url="https://api.razorpay.com/v1/payments",
-			# 		auth=(
-			# 			self.api_key,
-			# 			self.get_password(fieldname="api_secret", raise_exception=False),
-			# 		),
-			# 	)
-			# except Exception:
-			# 	frappe.throw(_("Seems API Key or API Secret is wrong !!!"))
 
 	def validate_transaction_currency(self, currency):
 		if currency not in self.supported_currencies:
@@ -47,42 +25,9 @@ class LetzpaySettings(Document):
 				).format(currency)
 			)
 
-
 	def get_payment_url(self, **kwargs):
-		integration_request = create_request_log(kwargs, integration_type='Host',service_name="Razorpay")
+		integration_request = create_request_log(kwargs, integration_type='Host',service_name="LetzPay")
 		return get_url(f"./letzpay_checkout?token={integration_request.name}")
-
-	def create_order(self, **kwargs):
-		# Creating Orders https://razorpay.com/docs/api/orders/
-
-		# convert rupees to paisa
-		kwargs["amount"] *= 100
-
-		# Create integration log
-		integration_request = create_request_log(kwargs, integration_type='Host' ,service_name="Razorpay")
-
-		# Setup payment options
-		payment_options = {
-			"amount": kwargs.get("amount"),
-			"currency": kwargs.get("currency", "INR"),
-			"receipt": kwargs.get("receipt"),
-			"payment_capture": kwargs.get("payment_capture"),
-		}
-		if self.api_key and self.api_secret:
-			try:
-				order = make_post_request(
-					"https://api.razorpay.com/v1/orders",
-					auth=(
-						self.api_key,
-						self.get_password(fieldname="api_secret", raise_exception=False),
-					),
-					data=payment_options,
-				)
-				order["integration_request"] = integration_request.name
-				return order  # Order returned to be consumed by razorpay.js
-			except Exception:
-				frappe.log(frappe.get_traceback())
-				frappe.throw(_("Could not create razorpay order"))
 
 	def create_request(self, data):
 		self.data = frappe._dict(data)
@@ -104,76 +49,6 @@ class LetzpaySettings(Document):
 				"status": 401,
 			}
 
-	def authorize_payment(self):
-		"""
-		An authorization is performed when user’s payment details are successfully authenticated by the bank.
-		The money is deducted from the customer’s account, but will not be transferred to the merchant’s account
-		until it is explicitly captured by merchant.
-		"""
-		data = json.loads(self.integration_request.data)
-		settings = self.get_settings(data)
-
-		try:
-			resp = make_get_request(
-				f"https://api.razorpay.com/v1/payments/{self.data.razorpay_payment_id}",
-				auth=(settings.api_key, settings.api_secret),
-			)
-
-			if resp.get("status") == "authorized":
-				self.integration_request.update_status(data, "Authorized")
-				self.flags.status_changed_to = "Authorized"
-
-			elif resp.get("status") == "captured":
-				self.integration_request.update_status(data, "Completed")
-				self.flags.status_changed_to = "Completed"
-
-			elif data.get("subscription_id"):
-				if resp.get("status") == "refunded":
-					# if subscription start date is in future then
-					# razorpay refunds the amount after authorizing the card details
-					# thus changing status to Verified
-
-					self.integration_request.update_status(data, "Completed")
-					self.flags.status_changed_to = "Verified"
-
-			else:
-				frappe.log_error(message=str(resp), title="Razorpay Payment not authorized")
-
-		except Exception:
-			frappe.log_error()
-
-		status = frappe.flags.integration_request.status_code
-
-		redirect_to = data.get("redirect_to") or None
-		redirect_message = data.get("redirect_message") or None
-		if self.flags.status_changed_to in ("Authorized", "Verified", "Completed"):
-			if self.data.reference_doctype and self.data.reference_docname:
-				custom_redirect_to = None
-				try:
-					frappe.flags.data = data
-					custom_redirect_to = frappe.get_doc(
-						self.data.reference_doctype, self.data.reference_docname
-					).run_method("on_payment_authorized", self.flags.status_changed_to)
-
-				except Exception:
-					frappe.log_error(frappe.get_traceback())
-
-				if custom_redirect_to:
-					redirect_to = custom_redirect_to
-
-			redirect_url = "payment-success?doctype={}&docname={}".format(
-				self.data.reference_doctype, self.data.reference_docname
-			)
-		else:
-			redirect_url = "payment-failed"
-
-		if redirect_to:
-			redirect_url += "&" + urlencode({"redirect_to": redirect_to})
-		if redirect_message:
-			redirect_url += "&" + urlencode({"redirect_message": redirect_message})
-
-		return {"redirect_to": redirect_url, "status": status}
-
 	def get_settings(self, data):
 		settings = frappe._dict(
 			{
@@ -192,97 +67,7 @@ class LetzpaySettings(Document):
 
 		return settings
 
-
-
-def capture_payment(is_sandbox=False, sanbox_response=None):
-	"""
-	Verifies the purchase as complete by the merchant.
-	After capture, the amount is transferred to the merchant within T+3 days
-	where T is the day on which payment is captured.
-
-	Note: Attempting to capture a payment whose status is not authorized will produce an error.
-	"""
-	controller = frappe.get_doc("Razorpay Settings")
-
-	for doc in frappe.get_all(
-		"Integration Request",
-		filters={"status": "Authorized", "integration_request_service": "Razorpay"},
-		fields=["name", "data"],
-	):
-		try:
-			if is_sandbox:
-				resp = sanbox_response
-			else:
-				data = json.loads(doc.data)
-				settings = controller.get_settings(data)
-
-				resp = make_get_request(
-					"https://api.razorpay.com/v1/payments/{}".format(data.get("razorpay_payment_id")),
-					auth=(settings.api_key, settings.api_secret),
-					data={"amount": data.get("amount")},
-				)
-
-				if resp.get("status") == "authorized":
-					resp = make_post_request(
-						"https://api.razorpay.com/v1/payments/{}/capture".format(
-							data.get("razorpay_payment_id")
-						),
-						auth=(settings.api_key, settings.api_secret),
-						data={"amount": data.get("amount")},
-					)
-
-			if resp.get("status") == "captured":
-				frappe.db.set_value("Integration Request", doc.name, "status", "Completed")
-
-		except Exception:
-			doc = frappe.get_doc("Integration Request", doc.name)
-			doc.status = "Failed"
-			doc.error = frappe.get_traceback()
-			doc.save()
-			frappe.log_error(doc.error, f"{doc.name} Failed")
-
 @frappe.whitelist(allow_guest=True)
 def get_api_key():
 	controller = frappe.get_doc("Letzpay Settings")
 	return controller.api_key
-
-@frappe.whitelist(allow_guest=True)
-def get_order(doctype, docname):
-	# Order returned to be consumed by razorpay.js
-	doc = frappe.get_doc(doctype, docname)
-	try:
-		# Do not use run_method here as it fails silently
-		return doc.get_razorpay_order()
-	except AttributeError:
-		frappe.log_error(
-			frappe.get_traceback(), _("Controller method get_razorpay_order missing")
-		)
-		frappe.throw(_("Could not create Razorpay order. Please contact Administrator"))
-
-
-@frappe.whitelist(allow_guest=True)
-def order_payment_success(integration_request, params):
-	"""Called by razorpay.js on order payment success, the params
-	contains razorpay_payment_id, razorpay_order_id, razorpay_signature
-	that is updated in the data field of integration request
-
-	Args:
-	        integration_request (string): Name for integration request doc
-	        params (string): Params to be updated for integration request.
-	"""
-	params = json.loads(params)
-	integration = frappe.get_doc("Integration Request", integration_request)
-
-	# Update integration request
-	integration.update_status(params, integration.status)
-	integration.reload()
-
-	data = json.loads(integration.data)
-	controller = frappe.get_doc("Razorpay Settings")
-
-	# Update payment and integration data for payment controller object
-	controller.integration_request = integration
-	controller.data = frappe._dict(data)
-
-	# Authorize payment
-	controller.authorize_payment()
--- letzpay_integration/www/letzpay_checkout/index.py
+++ letzpay_integration/www/letzpay_checkout/index.py
@@ -1,19 +1,26 @@
 import frappe
 import hashlib
+from frappe.utils import cint
 
 def get_context(context):
-
-    SALT = "4e9807cfc9ed4933" #Provided by Letzpay
     data={}
-    data.update({'PAY_ID':'1071521128151253'}) #Provided by Letzpay
+    query_params = frappe.form_dict
+    integraton_req = frappe.get_doc('Integration Request',query_params.get('token'))
+    letz_perams = frappe.get_doc('Letzpay Settings')
+    SALT = letz_perams.salt #Provided by Letzpay
+    payment_req_doc = frappe.get_doc('Payment Request',integraton_req.reference_docname)
+    print(payment_req_doc.as_dict(),"HJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJj")
+    sales_doc = frappe.get_doc('Sales Invoice', payment_req_doc.reference_name )
+    contact_detail = frappe.get_doc('Contact', sales_doc.contact_person)
+    data.update({'PAY_ID':letz_perams.pay_id}) #Provided by Letzpay
     data.update({'ORDER_ID':frappe.form_dict.token})
-    data.update({'AMOUNT':'100'})
-    data.update({'TXNTYPE':'SALE'})
-    data.update({'CUST_NAME':'Amitosh'})
-    data.update({'CUST_EMAIL':'amitosh@letzpay.com'})
-    data.update({'CUST_PHONE':'7077626024'})
-    data.update({'CUST_ID':'TEST1234567890'})
-    data.update({'CURRENCY_CODE':'356'})
+    data.update({'AMOUNT':cint(sales_doc.grand_total * 100)})
+    data.update({'TXNTYPE':letz_perams.txntype})
+    data.update({'CUST_NAME':sales_doc.customer_name})
+    data.update({'CUST_EMAIL':contact_detail.email_id})
+    data.update({'CUST_PHONE':contact_detail.mobile_no})
+    data.update({'CUST_ID':sales_doc.customer})
+    data.update({'CURRENCY_CODE':letz_perams.currency_code})
     data.update({'RETURN_URL':'http://127.0.0.1:8003/api/method/letzpay_integration.www.letzpay_checkout.index.get_api_data'}) #Merchant's return URL
     hashString=''
     for key in sorted(data.keys()):
@@ -24,7 +31,7 @@ def get_context(context):
     hashh = hashlib.sha256(finalHashString.encode())
     finalHash = hashh.hexdigest().upper()
     data.update({'hash':finalHash})
-    # print(data, '>>>>>>>>')
+    print(data, '>>>>>>>>')
     context.data = data
     context.action = action
 
@@ -36,6 +43,7 @@ def get_api_data(**kwargs):
     frappe.get_doc({"doctype":'Integration Request',
                     "integration_type":"Host",
                     "data":json.dumps(kwargs),
+                    "integration_request_service":"LetzPay",
                     "status":"Authorized",
                     "reference_doctype": doc.reference_doctype,
                     "reference_docname":doc.reference_docname
